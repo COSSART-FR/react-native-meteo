81f89610516f00bfa297ad218e6b98d2571d3085
COSSART <mathis.cossart@gmail.com>
2023-12-15 14:29:06 +0100

    Initialisation

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { s } from "./App.style";
+import Home from "./pages/Home/Home";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <Home />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
